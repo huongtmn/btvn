8a2d3ab6f39566b5646fe8723b9b9d1aaa318861
huongtmn <huongnguyen.aud@gmail.com>
2022-12-16 10:00:33 +0700

    BTVN Component1

---
+++ src/BaiTapThucHanhLayout/Item.jsx
@@ -0,0 +1,84 @@
+import React, { Component } from "react";
+
+export default class Item extends Component {
+  render() {
+    return (
+        <div>
+            <header className="py-5">
+                <div className="container px-lg-5">
+                    <div className="row text-center">
+                        <div className="col-sm-3">
+                            <div className="card">
+                                <img src={'/500x325.png'} className="card-img-top" alt="..." />
+                                <div className="card-body">
+                                    <h5 className="card-title">Card title</h5>
+                                    <p className="card-text">
+                                    Some quick example text to build on the card title and make up the
+                                    bulk of the card's content.
+                                    </p>                                 
+                                </div>
+                                <div className="card-footer text-muted">
+                                    <a href="#" className="btn btn-primary">
+                                    Find Out More!
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-sm-3">
+                            <div className="card">
+                                <img src={'/500x325.png'} className="card-img-top" alt="..." />
+                                <div className="card-body">
+                                    <h5 className="card-title">Card title</h5>
+                                    <p className="card-text">
+                                    Some quick example text to build on the card title and make up the
+                                    bulk of the card's content.
+                                    </p>                                 
+                                </div>
+                                <div className="card-footer text-muted">
+                                    <a href="#" className="btn btn-primary">
+                                    Find Out More!
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-sm-3">
+                            <div className="card">
+                                <img src={'/500x325.png'} className="card-img-top" alt="..." />
+                                <div className="card-body">
+                                    <h5 className="card-title">Card title</h5>
+                                    <p className="card-text">
+                                    Some quick example text to build on the card title and make up the
+                                    bulk of the card's content.
+                                    </p>                                 
+                                </div>
+                                <div className="card-footer text-muted">
+                                    <a href="#" className="btn btn-primary">
+                                    Find Out More!
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-sm-3">
+                            <div className="card">
+                                <img src={'/500x325.png'} className="card-img-top" alt="..." />
+                                <div className="card-body">
+                                    <h5 className="card-title">Card title</h5>
+                                    <p className="card-text">
+                                    Some quick example text to build on the card title and make up the
+                                    bulk of the card's content.
+                                    </p>                                 
+                                </div>
+                                <div className="card-footer text-muted">
+                                    <a href="#" className="btn btn-primary">
+                                    Find Out More!
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </header> 
+        </div>
+    );
+  }
+}
